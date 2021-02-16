fb688fc56c5bb58179024c41e5457bbf41dd455b
thisLucio <luxusnietzsche@gmail.com>
2021-02-16 13:11:03 -0300

    Finalização Menu Navegação

--- exercicio.php
+++ exercicio.php
@@ -17,14 +17,14 @@
           <h2>Visualização do Exercício</h2>
       </header>
       <nav class="navegacao">
-          <a href="#" class="verde">Sem formatação</a>
+          <a href="<?="{$_GET['dir']}/{$_GET['file']}.php"?>" class="verde">Sem formatação</a>
           <a href="index.php" class="vermelho">Voltar</a>
         </nav>
       <main class="principal">
           <div class="conteudo">
                 <?php 
                     //include($_GET['dir'] . "/". $_GET['file'] . ".php");
-                    include("{$_GET['dir']}/{$_GET['file']}.php");
+                    include(__DIR__ . "/{$_GET['dir']}/{$_GET['file']}.php");
                 ?>
           </div>
       </main>
