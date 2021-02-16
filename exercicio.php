cea1fefb32fa0e489afa041d3a5b74a1ad906d0f
thisLucio <luxusnietzsche@gmail.com>
2021-02-16 12:45:07 -0300

    Teste GET

--- exercicio.php
+++ exercicio.php
@@ -7,6 +7,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets\css\estilo.css">
+    <link rel="stylesheet" href="assets\css\exercicio.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Exerício</title>
 </head>
@@ -21,7 +22,10 @@
         </nav>
       <main class="principal">
           <div class="conteudo">
-
+                <?php 
+                    //include($_GET['dir'] . "/". $_GET['file'] . ".php");
+                    include("{$_GET['dir']}/{$_GET['file']}.php");
+                ?>
           </div>
       </main>
       <footer class="rodape">
